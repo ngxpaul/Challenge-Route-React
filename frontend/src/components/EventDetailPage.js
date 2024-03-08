728c19ae5357c877dc4acfd4766b90e423ab579f
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-08 16:41:54 +0700

    base

---
+++ frontend/src/components/EventDetailPage.js
@@ -0,0 +1,6 @@
+function EditEventPage() {
+  return <div>
+    <h1>Edit Event Page</h1>
+  </div>;
+}
+export default EditEventPage;
---
+++ frontend/src/components/NewEventPage.js
@@ -0,0 +1,8 @@
+function NewEventPage(){
+    return (
+        <h1>
+            New Event Page
+        </h1>
+    )
+}
+export default NewEventPage;    
