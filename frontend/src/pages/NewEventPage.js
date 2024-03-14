d6a1a3dd0da4cf1aa60543b44fd5819752e3c332
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-14 14:15:29 +0700

    ad d new Event

--- frontend/src/pages/NewEventPage.js
+++ frontend/src/pages/NewEventPage.js
@@ -1,8 +1,7 @@
+import EventForm from "../components/EventForm";
 function NewEventPage(){
     return (
-        <h1>
-            New Event Page
-        </h1>
+        <EventForm />
     )
 }
 export default NewEventPage;    
