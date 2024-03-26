af46925c53cae8d31bcad83e04d47c205a5d9061
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-26 17:20:42 +0700

    Change Bton submitting when press Save

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,36 +1,66 @@
-import { useNavigate ,Form} from 'react-router-dom';
+import { useNavigate, Form } from "react-router-dom";
 
-import classes from './EventForm.module.css';
-
+import classes from "./EventForm.module.css";
+import { useNavigation } from "react-router-dom";
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigate.state === "submitting";
+
   function cancelHandler() {
-    navigate('..');
+    navigate("..");
   }
 
   return (
-    <Form method='post' className={classes.form}>
+    <Form method="post" className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required  defaultValue={event ? event.title : ''}/>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          required
+          defaultValue={event ? event.title : ""}
+        />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required defaultValue={event ? event.image : ''} />
+        <input
+          id="image"
+          type="url"
+          name="image"
+          required
+          defaultValue={event ? event.image : ""}
+        />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required defaultValue={event ? event.date : ''} />
+        <input
+          id="date"
+          type="date"
+          name="date"
+          required
+          defaultValue={event ? event.date : ""}
+        />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ''} />
+        <textarea
+          id="description"
+          name="description"
+          rows="5"
+          required
+          defaultValue={event ? event.description : ""}
+        />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button disabled={isSubmitting} type="button" onClick={cancelHandler}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Sumitting..." : "Save"}
+        </button>
       </div>
     </Form>
   );
