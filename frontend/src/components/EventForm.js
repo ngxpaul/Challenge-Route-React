c8bb7dca7482e550f7d52af3c889b3a93afbe3cb
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-28 08:48:25 +0700

    fix button submitt

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -6,7 +6,7 @@ function EventForm({ method, event }) {
   const navigate = useNavigate();
   const navigation = useNavigation();
 
-  const isSubmitting = navigate.state === "submitting";
+  const isSubmitting = navigation.state === "submitting";
 
   function cancelHandler() {
     navigate("..");
